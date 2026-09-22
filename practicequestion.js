b30ffa1a1db78d09974ca18c16ed51d3e0a0d4c1
Prangshu12 <prangshu12@gmail.com>
2026-09-22 16:58:31 +0530

    First question done

---
+++ practicequestion.js
@@ -0,0 +1,44 @@
+// ### 1. Create a JavaScript program that stores information about a student.
+// **(DATE - 19/08/2026)**
+// Your program should store:
+// - Student name
+// - Age
+// - Course
+// - College name
+// - Marks
+// - Birth year
+// Then display all the information in the console.
+// ### Requirements
+// 1. Use `let`, `const`, and `var` in your program.
+// 2. Change at least **two values** after declaring them.
+// 3. Try changing a value that should not be changed.
+// 4. Observe what happens.
+// 5. Print the final values to the console.
+// ### ⭐ Bonus ⭐
+// Add:
+// - City
+// - Semester
+// - Favourite subject
+
+
+const Student_name = "Prangshu Pranjal Saikia";
+let age = 18;
+let course = "BTech Cse"
+var College_name = "SITM";
+let Marks = 80 + "%";
+const Birth_year = 2008;
+age = 19;
+Marks = 80.5 + "%";
+let City = "Golaghat";
+let Semester = 1 + "st";
+let Favourite_subject = "coding";
+
+console.log("Student name: " + Student_name);
+console.log("Student age: " + age);
+console.log("Student course: " + course);
+console.log("College name: " + College_name);
+console.log("Student's perfomance: " + Marks);
+console.log("Birth year: " + Birth_year);
+console.log("City: " + City);
+console.log("Semester: " + Semester);
+console.log("Student's subject preference: " + Favourite_subject);
